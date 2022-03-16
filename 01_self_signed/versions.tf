49e96003e560e7720c4c5bdf63fb98ffc0e56e71
Xavier Mignot <xavier.mignot@floa.com>
2022-03-16 22:39:25 +0100

    chore: Use versions.tf file only if necessary

--- 01_self_signed/versions.tf
+++ 01_self_signed/versions.tf
@@ -1,13 +1,5 @@
 terraform {
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 3.0"
-    }
     pkcs12 = {
       source  = "chilicat/pkcs12"
       version = "~> 0.0"
--- versions.tf
+++ versions.tf
@@ -1,14 +1,33 @@
 terraform {
   required_providers {
+    acme = {
+      source  = "vancluever/acme"
+      version = "~> 2.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 2.0"
+    }
+    pkcs12 = {
+      source  = "chilicat/pkcs12"
+      version = "~> 0.0"
     }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.0"
+    }
   }
+}
+
+provider "acme" {
+  # staging
+  # server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  # production
+  server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
 
 provider "azurerm" {
