44363627bf41830c832ddd6aa35a826f2c6d6b5a
Xavier Mignot <xavier.mignot@floa.com>
2022-03-16 22:39:01 +0100

    chore: Renaming

--- 00_app_service_cert/main.tf
+++ 00_app_service_cert/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service_certificate" "self_signed" {
+resource "azurerm_app_service_certificate" "cert" {
   name                = "self-signed"
   resource_group_name = var.resource_group_name
   location            = var.location
@@ -7,8 +7,8 @@ resource "azurerm_app_service_certificate" "self_signed" {
   password = var.pfx_password
 }
 
-resource "azurerm_app_service_certificate_binding" "self_signed" {
+resource "azurerm_app_service_certificate_binding" "cert_binding" {
   hostname_binding_id = var.custom_domain_binding_id
-  certificate_id      = azurerm_app_service_certificate.self_signed.id
+  certificate_id      = azurerm_app_service_certificate.cert.id
   ssl_state           = "SniEnabled"
 }
