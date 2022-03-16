a73dee41125c2dd8dce1f9cf903b30a7e921ed39
Xavier Mignot <xavier.mignot@floa.com>
2022-03-16 23:19:01 +0100

    chore: Remove not needed providers from root module

--- versions.tf
+++ versions.tf
@@ -1,16 +1,8 @@
 terraform {
   required_providers {
-    acme = {
-      source  = "vancluever/acme"
-      version = "~> 2.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 2.0"
-    }
-    pkcs12 = {
-      source  = "chilicat/pkcs12"
-      version = "~> 0.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -21,13 +13,6 @@ terraform {
       version = "~> 3.0"
     }
   }
-}
-
-provider "acme" {
-  # staging
-  # server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
-  # production
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
 
 provider "azurerm" {
