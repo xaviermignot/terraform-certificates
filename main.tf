c3c66702a267686fe7514d2a0703ebaa18c2a98f
Xavier Mignot <code@xmi.fr>
2024-03-12 09:25:06 -0400

    feat: Add a variable to choose module to use

--- main.tf
+++ main.tf
@@ -16,6 +16,8 @@ module "app_service" {
 
 # This module generates a self-signed certificate and creates an App Service certificate from it
 module "self_signed" {
+  count = var.module_to_use == "self_signed" ? 1 : 0
+
   source = "./01_self_signed"
 
   resource_group_name = module.app_service.resource_group_name
@@ -25,6 +27,8 @@ module "self_signed" {
 
 # This module gets a certificate from Let's Encrypt and creates an App Service certificate from it
 module "acme" {
+  count = var.module_to_use == "acme" ? 1 : 0
+
   source = "./02_acme"
 
   resource_group_name = module.app_service.resource_group_name
@@ -36,16 +40,25 @@ module "acme" {
     zone_name    = var.dns_zone_name
     zone_rg_name = var.dns_zone_rg_name
   }
+
+  providers = {
+    acme = acme
+    # acme = acme.staging
+  }
 }
 
 # This module creates an App Service managed certificate
 module "managed" {
+  count = var.module_to_use == "managed" ? 1 : 0
+
   source = "./03_managed"
 
   custom_domain_binding_id = module.app_service.custom_domain_binding_id
 }
 
 module "key_vault" {
+  count = var.module_to_use == "key_vault" ? 1 : 0
+
   source = "./04_key_vault"
 
   resource_group_name = module.app_service.resource_group_name
@@ -55,10 +68,19 @@ module "key_vault" {
   email               = "contact@${var.dns_zone_name}"
 }
 
+locals {
+  certificate_id = {
+    self_signed = var.module_to_use == "self_signed" ? module.self_signed[0].certificate_id : ""
+    acme        = var.module_to_use == "acme" ? module.acme[0].certificate_id : ""
+    managed     = var.module_to_use == "acme" ? module.acme[0].certificate_id : ""
+    key_vault   = var.module_to_use == "key_vault" ? module.key_vault[0].certificate_id : ""
+  }[var.module_to_use]
+}
+
 # The binding between the App Service custom domain and the certificate is done here.
 # You can choose which certificate is used and see the result after applying the changes
 resource "azurerm_app_service_certificate_binding" "cert_binding" {
-  certificate_id      = module.managed.certificate_id
+  certificate_id      = local.certificate_id
   hostname_binding_id = module.app_service.custom_domain_binding_id
   ssl_state           = "SniEnabled"
 }
--- variables.tf
+++ variables.tf
@@ -14,3 +14,13 @@ variable "dns_zone_rg_name" {
   description = "The name of the resource group containing the DNS Zone"
   default     = "rg-dns"
 }
+
+variable "module_to_use" {
+  type        = string
+  description = "The name of the module to use"
+  default     = "managed"
+  validation {
+    condition     = contains(["self_signed", "acme", "managed", "key_vault"], var.module_to_use)
+    error_message = "Variable module_to_use must be either 'self_signed', 'acme', 'managed' or 'key_vault'."
+  }
+}
